ddbc3a81ffa48fb6b5b116306bfcb7e3ea629e06
Terje Sylvarnes <fuzzklang@gmail.com>
2023-08-01 16:48:32 +0200

    chore(go): remove unnecessary line

--- wc-go/wc-go/wc.go
+++ wc-go/wc-go/wc.go
@@ -13,8 +13,6 @@ func CountAll(r io.Reader) (line_count int, word_count int, char_count int, byte
 		byte_count += len(sc.Bytes()) + 1 // + 1 for removed newline
 		char_count += len(sc.Text()) + 1
 		word_count += len(strings.Fields(sc.Text()))
-
-		_ = sc.Text()
 	}
 	err = nil
 	return
